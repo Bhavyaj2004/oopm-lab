355ebd8fdd2db56742f6f71fc2b0253597258e6c
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2023-10-16 12:43:51 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Debugger.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Debug" button to debug it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+void print(int a){
+    cout<<"ÿour number is :"<<a;
+}
+int main(){
+    cout<<"Bhavya Jain \n0827CI221043";
+    void print(int);// declearing a function
+    int a;
+    cout<<"Ënter any number:";
+    cin>>a;
+    print(a);
+    
+
+    return 0;
+}
