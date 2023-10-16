b179e17d49499ea0166de04bd2e94072e908c9e8
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2023-10-16 12:42:06 +0530

    Add files via upload

---
+++ oopm 3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void print(int n){
+	cout<<"Your number is :"<<n; // defining a function
+}
+int main(){
+	cout<<"Bhavya Jain\n0827CI221043\n";
+	void print(int); // declearatio of a function
+	int n;
+	cout<<"Enter a number :";
+	cin>>n;
+	print(n); //calling a function
+}
